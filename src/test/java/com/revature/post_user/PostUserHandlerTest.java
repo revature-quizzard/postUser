264b75daa73e085c7014ad5a1da44ac87b6b967a
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-28 13:56:22 -0500

    Edited the POM to allow for testing dependencies.

---
+++ src/test/java/com/revature/post_user/PostUserHandlerTest.java
@@ -0,0 +1,18 @@
+package com.revature.post_user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostUserHandlerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleRequest() {
+    }
+}
